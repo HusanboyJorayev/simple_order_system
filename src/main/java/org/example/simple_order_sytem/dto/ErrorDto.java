4ea889c91ef4bdc3a494a60a10701325f5d5ae87
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-07-19 13:50:50 +0500

    added variables application properties file

--- src/main/java/org/example/simple_order_sytem/dto/ErrorDto.java
+++ src/main/java/org/example/simple_order_sytem/dto/ErrorDto.java
@@ -1,12 +1,18 @@
 package org.example.simple_order_sytem.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
+
+import lombok.*;
+
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@ToString
 public class ErrorDto {
     private String message;
     private String code;
